2b423a8d643c30de6ec607bad5ecf612090c6d22
Bekah Hawrot Weigel <rhawrot@gmail.com>
2017-12-22 19:44:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+
+if sayHiToGrandma(toUpperCase) {
+  console.log(“YES INDEED!”)
+} else {
+  console.log("I can’t hear you!")
+}
+
+
+function sayHiToGrandma("I love you, Grandma.") {
+ return ${"I love you too!"}
+}
+
